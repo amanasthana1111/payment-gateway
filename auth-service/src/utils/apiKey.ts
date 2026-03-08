e859a79fa3657ab877589b174d130505aae086ca
amanasthana1111 <as.asthana.1111@gmail.com>
2026-03-08 15:49:40 +0530

    uuid based api and secret code generated

--- auth-service/src/utils/apiKey.ts
+++ auth-service/src/utils/apiKey.ts
@@ -1,9 +1,9 @@
 import { v4 as uuidv4 } from 'uuid';
 
 export function generateApiKey(): string {
-  return `ak_${uuidv4().replace(/-/g, '')}`;
+  return `aman_pay_ak_${uuidv4().replace(/-/g, '')}`;
 }
 
 export function generateApiSecret(): string {
-  return `sk_${uuidv4().replace(/-/g, '')}`;
+  return `aman_pay_sk_${uuidv4().replace(/-/g, '')}`;
 }
